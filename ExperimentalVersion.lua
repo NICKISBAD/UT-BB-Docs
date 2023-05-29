a1576c15414144e83be6b569b6967c7f04cb8c79
Nick <107813557+NICKISBAD@users.noreply.github.com>
2023-05-29 14:10:05 +0500

    Testing

--- ExperimentalVersion.lua
+++ ExperimentalVersion.lua
@@ -64,6 +64,61 @@ Tab:AddLabel("2/3: Which is why VIPs are recommended, Your INF Damage")
 Tab:AddLabel("2.5/3:may go for another player's priority and miss")
 Tab:AddLabel("3/3: P1 = Dummy, P2 = Dummy2, P3 = Dummy3")
 
+Tab:AddLabel(" ")
+Tab:AddLabel("Key binds")
+
+Tab:AddBind({
+    Name = "Smite P1",
+	Default = Enum.KeyCode.H,
+	Hold = false,
+	Callback = function()
+	    GSmite(game.Workspace.Game.Mobs:WaitForChild("Dummy"))
+	end
+})
+
+Tab:AddBind({
+    Name = "Smite P2",
+	Default = Enum.KeyCode.H,
+	Hold = false,
+	Callback = function()
+	    GSmite(game.Workspace.Game.Mobs:WaitForChild("Dummy2"))
+	end
+})
+
+Tab:AddBind({
+    Name = "Smite P3",
+	Default = Enum.KeyCode.H,
+	Hold = false,
+	Callback = function()
+	    GSmite(game.Workspace.Game.Mobs:WaitForChild("Dummy3"))
+	end
+})
+
+local P1 = game.Workspace.Game.Mobs:WaitForChild("Dummy")
+local P2 = game.Workspace.Game.Mobs:WaitForChild("Dummy2")
+local P3 = game.Workspace.Game.Mobs:WaitForChild("Dummy3")
+
+
+Tab:AddBind({
+    Name = "Smite all priorities",
+	Default = Enum.KeyCode.H,
+	Hold = false,
+	Callback = function()
+	    GSmite(P1)
+		GSmite(P2)
+		GSmite(P3)
+	end
+})
+
+Tab:AddButton({
+    Name = "Smite all (mobile)",
+    Callback = function()
+        GSmite(P1)
+		GSmite(P2)
+		GSmite(P3)
+	end
+})
+
 
 local Tab = Window:MakeTab({ 	Name = "Player", 	Icon = "rbxassetid://4483345998", 	PremiumOnly = false }) 
 
@@ -150,7 +205,7 @@ Tab:AddButton({
    end
 })
 
-local Tab = Window:MakeTab({ 	Name = "Attacks", 	Icon = "rbxassetid://4483345998", 	PremiumOnly = false })
+Tab:AddLabel("Attacks")
 
 local Dmg = {
   DMGVal = 0
@@ -203,6 +258,8 @@ Tab:AddButton({
  game:GetService("ReplicatedStorage").ArmorShoot:FireServer(unpack(args))
     end
 })
+
+Tab:AddLabel("Spears")
 
 Tab:AddButton({
     Name = "999999 Spears P1",
@@ -311,62 +368,6 @@ Tab:AddButton({
     br.Parent = game.ReplicatedStorage
  end
 })
-			
-local Tab = Window:MakeTab({ 	Name = "Binds", 	Icon = "rbxassetid://4483345998", 	PremiumOnly = false })
-
-Tab:AddBind({
-    Name = "Smite P1",
-	Default = Enum.KeyCode.H,
-	Hold = false,
-	Callback = function()
-	    GSmite(game.Workspace.Game.Mobs:WaitForChild("Dummy"))
-	end
-})
-
-Tab:AddBind({
-    Name = "Smite P2",
-	Default = Enum.KeyCode.H,
-	Hold = false,
-	Callback = function()
-	    GSmite(game.Workspace.Game.Mobs:WaitForChild("Dummy2"))
-	end
-})
-
-Tab:AddBind({
-    Name = "Smite P3",
-	Default = Enum.KeyCode.H,
-	Hold = false,
-	Callback = function()
-	    GSmite(game.Workspace.Game.Mobs:WaitForChild("Dummy3"))
-	end
-})
-
-local P1 = game.Workspace.Game.Mobs:WaitForChild("Dummy")
-local P2 = game.Workspace.Game.Mobs:WaitForChild("Dummy2")
-local P3 = game.Workspace.Game.Mobs:WaitForChild("Dummy3")
-
-
-Tab:AddBind({
-    Name = "Smite all priorities",
-	Default = Enum.KeyCode.H,
-	Hold = false,
-	Callback = function()
-	    GSmite(P1)
-		GSmite(P2)
-		GSmite(P3)
-	end
-})
-
-Tab:AddButton({
-    Name = "Smite all (mobile)",
-    Callback = function()
-        GSmite(P1)
-		GSmite(P2)
-		GSmite(P3)
-	end
-})
-
-Tab:AddLabel("Extras")
 
 
    
