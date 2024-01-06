11ea839142d58cef34b7cec97914f056a0a21f7f
Nick <107813557+NICKISBAD@users.noreply.github.com>
2024-01-06 15:00:44 +0500

    Update Some.lua

--- Some.lua
+++ Some.lua
@@ -96,7 +96,7 @@ spawn(function()
 	game:GetService("RunService").RenderStepped:Connect(function()
 		if _G.AutoSFOTH then
 			for i,v in pairs(game.Workspace:GetChildren()) do
-				if v.Name:match("Fighter") or v.Name:match("Master") or v.Name:match("SHEDLETSKY") then
+				if v.Name:match("Fighter") or table.find({"Sword Master", "Shedletsky"}, v.Name) then
 					CannonDamage(v:WaitForChild("HumanoidRootPart").Position)
 				end
 			end
